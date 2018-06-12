fe4b5d93733ef82576968c1a86a0dff406c3537a
Xer0 <tylergrayx@gmail.com>
2018-06-12 18:27:28 -0400

    Registration Form Working

--- src/components/landingpage/loginform.js
+++ src/components/landingpage/loginform.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import {loginUser} from '../actions/actions';
-import { connect } from 'react-redux';
-// import {reduxForm, Field} from 'redux-form';
+// import { connect } from 'react-redux';
+import {reduxForm, Field} from 'redux-form';
+
 export class LoginForm extends React.Component {
 
-handleSubmit = (event) => {
-  event.preventDefault();
-  this.props.dispatch(loginUser());
-}
+  onSubmit = () => {
+    this.props.dispatch(loginUser());
+  }
   render(){
   return (
     <div>
     <h2>Login</h2>
-    <form className='login-form' onSubmit={this.handleSubmit}>
+    <form className='login-form' onSubmit={this.props.handleSubmit(this.onSubmit)}>
         <div>
   	        <label htmlFor="user-name">Username </label>
-            <input type="text" name='Enter Username' id='loginUsername' placeholder='Username' />
+            <Field component="input" type="text" name='loginusername' id='loginUsername' placeholder='Username' />
         </div>
         <div>
             <label htmlFor="password">Password </label>
-            <input type="password" name='password' id='loginPassword' placeholder='Password' />
+            <Field component="input" type="password" name='loginpassword' id='loginPassword' placeholder='Password' />
         </div>
         <button type='submit'>Submit Login</button>
         </form>
@@ -29,5 +29,6 @@ handleSubmit = (event) => {
 }
 
 
-export default connect()(LoginForm);
+// export default connect()(LoginForm);
 
+export default reduxForm({ form: "login" })(LoginForm);
--- src/components/landingpage/registerform.js
+++ src/components/landingpage/registerform.js
@@ -1,38 +1,136 @@
 import React from 'react';
-import {loginUser} from '../actions/actions';
-import { connect } from 'react-redux';
-// import {reduxForm, Field} from 'redux-form';
+// import {loginUser} from '../actions/actions';
+// import { connect } from 'react-redux';
+import {reduxForm, Field, SubmissionError, focus} from 'redux-form';
+import {required, nonEmpty} from '../validation';
+import RegisterInput from './registerinput';
+
 export class RegisterForm extends React.Component {
 
-handleSubmit = (event) => {
-  event.preventDefault();
-  this.props.dispatch(loginUser());
 
-}
+  onSubmit(values) {
+
+    const APIURL = "http://localhost:8888/users";
+
+    return fetch(APIURL, {
+      method: 'POST',
+      body: JSON.stringify(values),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          if (
+            res.headers.has('content-type') &&
+            res.headers
+              .get('content-type')
+              .startsWith('application/json')
+          ) {
+            // It's a nice JSON error returned by us, so decode it
+              return res.json().then(err => Promise.reject(err));
+            }
+              // It's a less informative error returned by express
+            return Promise.reject({
+              code: res.status,
+              message: res.statusText
+            });
+        }
+              return;
+            })
+
+      .then(() => console.log('Submitted with values', values))
+      .catch(err => {
+        const {reason, message, location} = err;
+          if (reason === 'ValidationError') {
+            // Convert ValidationErrors into SubmissionErrors for Redux Form
+            return Promise.reject(
+              new SubmissionError({
+                  [location]: message
+                        })
+            );
+          }
+          return Promise.reject(
+            new SubmissionError({
+              _error: 'Error submitting message'
+            })
+          );
+      });
+
+
+  }
   render(){
 
+
+        let successMessage;
+        if (this.props.submitSucceeded) {
+            successMessage = (
+                <div className="message message-success">
+                    Registration Successful
+                </div>
+            );
+        }
+
+        let errorMessage;
+        if (this.props.error) {
+            errorMessage = (
+                <div className="message message-error">{this.props.error}</div>
+            );
+        }
   return (
     <div>
     <h2>Register</h2>
-    <form className='signup-form' onSubmit={this.handleSubmit}>
+    <form className='signup-form' onSubmit={this.props.handleSubmit(values => 
+      this.onSubmit(values)
+    )}>
+      {successMessage}
+      {errorMessage}
      	<div>
             <label htmlFor="nick-name">Nickname </label>
-            <input placeholder='This shows on posts' type="text" name='nick-name' id='registerNickname' />
+            <Field 
+              component={RegisterInput} 
+              type="text" 
+              name='nickname' 
+              id='registerNickname' 
+              placeholder='Nickname' 
+              validate={[required, nonEmpty]}
+            />
         </div>
         <div>
         	<label htmlFor="user-name">Username </label>
-            <input type="text" name='Enter Username' id='registerUsername' placeholder='Username' />
+            <Field 
+              component={RegisterInput} 
+              type="text" 
+              name='username' 
+              id='registerUsername' 
+              placeholder='Username'
+              validate={[required, nonEmpty]}
+            />
         </div>
         <div>
            	<label htmlFor="password">Password </label>
-            <input type="password" name='password' id='registerPassword' placeholder='Password' />
+            <Field 
+              component={RegisterInput} 
+              type="password" 
+              name='password' 
+              id='registerPassword' 
+              placeholder='Password'
+              validate={[required, nonEmpty]} 
+            />
         </div>
-            <button type='submit'>Submit Register </button>
+            <button type='submit' disabled={
+        this.props.pristine ||
+        this.props.submitting }>Submit Register 
+            </button>
         </form>
     </div>
   );
   }
 }
 
-export default connect()(RegisterForm);
+// export default connect()(RegisterForm);
 
+export default reduxForm({ 
+  form: "register",
+  onSubmitFail: (errors, dispatch) =>
+    dispatch(focus('register', Object.keys(errors)[0])) })(RegisterForm);
