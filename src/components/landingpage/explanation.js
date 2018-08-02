493b912012f51a654d8fbff5e5921adb5b8feca3
Xer0 <tylergrayx@gmail.com>
2018-08-02 13:32:46 -0400

    Removed excessive link guide instructions on landing page. Instead just pointed them to the Link Guide button that is available after logging in and clicking on the post button

--- src/components/landingpage/explanation.js
+++ src/components/landingpage/explanation.js
@@ -33,43 +33,45 @@ export default function Explanation() {
             </li>
 
             <li>
-              <p><span style={bold}>Step 3.</span> Click the post button at the top to post an image, gif, mp4, or youtube video by following the Link instructions below or click the "Link Guide" button after logged in.
+              <p><span style={bold}>Step 3.</span> Click the post button at the top to post an image, gif, mp4, or youtube video by following the instructions with the "Link Guide" button.
               </p>
             </li>
 
           </ul>
         </div>
-        <div className ="linkGuides">
-          <h2 style={color}>INSTRUCTIONS ON LINKS</h2>
-          <h3 style={color}>*Also available after login</h3>
-          <ul className="landingList">
-          
-            <li>
-              <p><span style={bold}>Youtube Link</span> Copy the url from the top of the browser on any youtube video. Correct examples below:
-              </p>
 
-              <p>Example url: https://www.youtube.com/watch?v=SN43u7up93I
-              </p>
-
-              <p>Mobile url: https:m.youtube.com/watch?v=SN43u7up93I
-              </p>
-            </li>
-
-            <li>
-              <p><span style={bold}>Image/Gif/mp4:</span> <span style={color2}>Desktop: </span> Google an image, gif, mp4, and right click on what you plan to post, and click "Copy Image Address", or if its an mp4, "Copy Video Address", "Copy Link Address" works depending on the site.
-              </p>
-            </li>
-
-            <li>
-                <p><span style={bold}>Image/Gif/mp4: </span><span style={color2}>Mobile </span> Just Google an image, gif, mp4, and press and hold your finger over your selected item, then select "Open image in new tab", then copy the link in the new address bar. "Copy Link Address" works depending on the site.
-                </p>
-                <p><span style={color2}>iPad/iPhone: </span> Similar as above, but when you hold your finger over a selected item, select "Copy", then paste into the address bar, hold your finger over the newly loaded image and select "Open image in new tab" and copy that url in the address bar.
-                </p>
-            </li>
-
-          </ul>
-        </div>
       </div>
     </section>
   );
 }
+
+        // <div className ="linkGuides">
+        //   <h2 style={color}>INSTRUCTIONS ON LINKS</h2>
+        //   <h3 style={color}>*Also available after login</h3>
+        //   <ul className="landingList">
+          
+        //     <li>
+        //       <p><span style={bold}>Youtube Link</span> Copy the url from the top of the browser on any youtube video. Correct examples below:
+        //       </p>
+
+        //       <p>Example url: https://www.youtube.com/watch?v=SN43u7up93I
+        //       </p>
+
+        //       <p>Mobile url: https:m.youtube.com/watch?v=SN43u7up93I
+        //       </p>
+        //     </li>
+
+        //     <li>
+        //       <p><span style={bold}>Image/Gif/mp4:</span> <span style={color2}>Desktop: </span> Google an image, gif, mp4, and right click on what you plan to post, and click "Copy Image Address", or if its an mp4, "Copy Video Address", "Copy Link Address" works depending on the site.
+        //       </p>
+        //     </li>
+
+        //     <li>
+        //         <p><span style={bold}>Image/Gif/mp4: </span><span style={color2}>Mobile </span> Just Google an image, gif, mp4, and press and hold your finger over your selected item, then select "Open image in new tab", then copy the link in the new address bar. "Copy Link Address" works depending on the site.
+        //         </p>
+        //         <p><span style={color2}>iPad/iPhone: </span> Similar as above, but when you hold your finger over a selected item, select "Copy", then paste into the address bar, hold your finger over the newly loaded image and select "Open image in new tab" and copy that url in the address bar.
+        //         </p>
+        //     </li>
+
+        //   </ul>
+        // </div>
